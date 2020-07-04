043de999bc1ca3f53047d47b0a800321d0d45482
Vikneshwar Thandeswaran <tvikneshwar@gmail.com>
2020-07-04 20:45:38 +0530

    Create Rain-sensor-interface-raw.ino

---
+++ Rain-sensor-interface-raw.ino
@@ -0,0 +1,43 @@
+
+//Link: https://www.instructables.com/id/Arduino-Modules-Rain-Sensor/
+/*
+        Interface/Wiring
+
+Rain Sensor ................. Arduino
+VCC............................... 5v
+GND.............................. GND
+A0.................................. Analog in 0
+
+
+*/
+
+
+// lowest and highest sensor readings:
+const int sensorMin = 0;     // sensor minimum
+const int sensorMax = 1024;  // sensor maximum
+
+void setup() {
+  // initialize serial communication @ 9600 baud:
+  Serial.begin(9600);  
+}
+void loop() {
+  // read the sensor on analog A0:
+	int sensorReading = analogRead(A0);
+  // map the sensor range (four options):
+  // ex: 'long int map(long int, long int, long int, long int, long int)'
+	int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+  
+  // range value:
+  switch (range) {
+ case 0:    // Sensor getting wet
+    Serial.println("Flood");
+    break;
+ case 1:    // Sensor getting wet
+    Serial.println("Rain Warning");
+    break;
+ case 2:    // Sensor dry - To shut this up delete the " Serial.println("Not Raining"); " below.
+    Serial.println("Not Raining");
+    break;
+  }
+  delay(1);  // delay between reads
+}
