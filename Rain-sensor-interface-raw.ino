fbd31a9fc928545bf4ce47bc822e2a52247a4e82
Vikneshwar Thandeswaran <tvikneshwar@gmail.com>
2020-07-07 10:42:51 +0530

    Update Rain-sensor-interface-raw.ino

--- Rain-sensor-interface-raw.ino
+++ Rain-sensor-interface-raw.ino
@@ -1,5 +1,5 @@
 
-//Link: https://www.instructables.com/id/Arduino-Modules-Rain-Sensor/
+//Vikneshwar Thandeswaran
 /*
         Interface/Wiring
 
@@ -11,33 +11,53 @@ A0.................................. Analog in 0
 
 */
 
+#include <SPI.h>
+#include <LoRa.h>
 
-// lowest and highest sensor readings:
 const int sensorMin = 0;     // sensor minimum
 const int sensorMax = 1024;  // sensor maximum
 
 void setup() {
-  // initialize serial communication @ 9600 baud:
-  Serial.begin(9600);  
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
 }
+
 void loop() {
+  Serial.print("Sending packet: ");
+  //Serial.println(counter);
   // read the sensor on analog A0:
-	int sensorReading = analogRead(A0);
+  int sensorReading = analogRead(A0);
   // map the sensor range (four options):
   // ex: 'long int map(long int, long int, long int, long int, long int)'
-	int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
-  
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+    // send packet
+  LoRa.beginPacket();
+  LoRa.print(sensorReading);
   // range value:
   switch (range) {
  case 0:    // Sensor getting wet
     Serial.println("Flood");
+    LoRa.print("Flood");
     break;
  case 1:    // Sensor getting wet
     Serial.println("Rain Warning");
+    LoRa.print("Rain Warning");
     break;
  case 2:    // Sensor dry - To shut this up delete the " Serial.println("Not Raining"); " below.
     Serial.println("Not Raining");
+    LoRa.print("Not Raining");
     break;
   }
-  delay(1);  // delay between reads
+  
+  LoRa.endPacket();
+  delay(9000);
+  
 }
+
