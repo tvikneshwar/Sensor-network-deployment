4cd29ca620435c0ba7e2b65347fd27b03bd3054c
Vikneshwar Thandeswaran <tvikneshwar@gmail.com>
2020-07-10 12:06:20 +0530

    Create Lora-node-rx-tx.ino

---
+++ Lora-node-rx-tx.ino
@@ -0,0 +1,44 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+String id;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver-Sender");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  //****************************************
+
+   int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    //Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      String LoRaData = LoRa.readString();
+      Serial.print(id +LoRaData); 
+      if(LoRaData == "R")
+  {
+    //digitalWrite(R1,HIGH);
+    Serial.print(LoRaData);
+    LoRa.print("hello ");
+  
+  } 
+    }
+    
+
+  //****************************************
+  
+    }
+   
+  }
